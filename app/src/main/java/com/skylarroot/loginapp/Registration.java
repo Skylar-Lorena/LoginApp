a50bd02a78a94894545592acf0c978202afbe9d3
SkyLoreCodes <lorayne911@gmail.com>
2022-01-23 19:21:05 +0300

    initialize firebase auth and set  input type for password

--- app/src/main/java/com/skylarroot/loginapp/Registration.java
+++ app/src/main/java/com/skylarroot/loginapp/Registration.java
@@ -1,18 +1,14 @@
 package com.skylarroot.loginapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Registration extends AppCompatActivity {
@@ -26,50 +22,46 @@ public class Registration extends AppCompatActivity {
         user_name=findViewById(R.id.username);
         pass_word=findViewById(R.id.password1);
         btn2_signup=findViewById(R.id.sign);
-        btn2_signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = user_name.getText().toString().trim();
-                String password= pass_word.getText().toString().trim();
-                if(email.isEmpty())
-                {
-                    user_name.setError("Email is empty");
-                    user_name.requestFocus();
-                    return;
-                }
-                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
-                {
-                    user_name.setError("Enter the valid email address");
-                    user_name.requestFocus();
-                    return;
-                }
-                if(password.isEmpty())
-                {
-                    pass_word.setError("Enter the password");
-                    pass_word.requestFocus();
-                    return;
-                }
-                if(password.length()<6)
-                {
-                    pass_word.setError("Length of the password should be more than 6");
-                    pass_word.requestFocus();
-                    return;
-                }
-                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful())
-                        {
-                            Toast.makeText(Registration.this,"You are successfully Registered", Toast.LENGTH_SHORT).show();
-                        }
-                        else
-                        {
-                            Toast.makeText(Registration.this,"You are not Registered! Try again",Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-
+        mAuth=FirebaseAuth.getInstance();
+        btn2_signup.setOnClickListener(v -> {
+            String email = user_name.getText().toString().trim();
+            String password= pass_word.getText().toString().trim();
+            if(email.isEmpty())
+            {
+                user_name.setError("Email is empty");
+                user_name.requestFocus();
+                return;
             }
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+            {
+                user_name.setError("Enter the valid email address");
+                user_name.requestFocus();
+                return;
+            }
+            if(password.isEmpty())
+            {
+                pass_word.setError("Enter the password");
+                pass_word.requestFocus();
+                return;
+            }
+            if(password.length()<6)
+            {
+                pass_word.setError("Length of the password should be more than 6");
+                pass_word.requestFocus();
+                return;
+            }
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(task -> {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(Registration.this,"You are successfully Registered", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(Registration.this, Login.class));
+                }
+                else
+                {
+                    Toast.makeText(Registration.this,"You are not Registered! Try again",Toast.LENGTH_SHORT).show();
+                }
+            });
+
         });
 
     }
